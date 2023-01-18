6d39893fdac4678747d661219d9244692de52a52
stinqyy <maxvik19m@mail.ru>
2023-01-18 20:19:49 +0300

    Выполнил третие задание

---
+++ task_3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*   Задача 15: Напишите программу, которая принимает на вход цифру,
+     обозначающую день недели, и проверяет, является ли этот день выходным.
+*/
+
+int GetNumber()
+{
+    Console.WriteLine("Ввидете число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberDay = GetNumber();
+
+if (numberDay == 1)
+{
+    Console.WriteLine("рабочий ден(");
+}
+if (numberDay == 2)
+{
+    Console.WriteLine("рабочий ден(");
+}
+if (numberDay == 3)
+{
+    Console.WriteLine("рабочий ден(");
+}
+if (numberDay == 4)
+{
+    Console.WriteLine("рабочий ден(");
+}
+if (numberDay == 5)
+{
+    Console.WriteLine("рабочий ден(");
+}
+if (numberDay == 6)
+{
+    Console.WriteLine("Выходной)");
+}
+if (numberDay == 7)
+{
+    Console.WriteLine("Выходной)");
+}
+if (numberDay > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
