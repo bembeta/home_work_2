cd83ed3ac28054a35e3998ea0bc602ab3bc13535
stinqyy <maxvik19m@mail.ru>
2023-01-18 19:35:35 +0300

    Выполнил первое задание

---
+++ task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*  Задача 10: Напишите программу, которая принимает на вход
+    трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+int GetNumber()
+{
+Console.WriteLine("Введите любое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int fnumber = GetNumber();
+{
+  Console.WriteLine((fnumber / 10) % 10); 
+}
