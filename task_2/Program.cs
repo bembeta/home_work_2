5356b8564e3c646c036c978deee4db54bd515565
stinqyy <maxvik19m@mail.ru>
2023-01-18 20:03:05 +0300

    Выполнил второе задание

---
+++ task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Задача 13: Напишите программу, которая выводит третью цифру
+    заданного числа или сообщает, что третьей цифры нет.
+ */
+
+ int GetNumber()
+ {
+    Console.WriteLine("Введите число до 999: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+ }
+
+int fnumber = GetNumber(); 
+{
+    if (fnumber >= 100) 
+    {
+        fnumber = fnumber % 10;
+        Console.WriteLine(fnumber);
+    }
+    else
+    {
+        Console.WriteLine("Число из двух цифр");
+    }
+}
